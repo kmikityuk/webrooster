db38597140233f1c7576c04763d8e3e87669399a
mikityuk <konstantin.mikityuk@psi.ch>
2021-04-10 19:07:47 +0200

    template and python script updated

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-#read template as a whole
+#read template
 f = open('template', mode = 'r')
 t0 = f.read()
 f.close()
@@ -6,12 +6,26 @@ f.close()
 #split
 t1 = t0.split('INSERTHERE')
 
-#read index as a whole
+#read index and write index.html
 f = open('index', mode = 'r')
 i0 = f.read()
 f.close()
-
-#write index.html
 f = open('index.html', mode = 'w')
+f.write(t1[0] + i0 + t1[1])
+f.close()
+
+#read structure and write structure.html
+f = open('structure', mode = 'r')
+i0 = f.read()
+f.close()
+f = open('structure.html', mode = 'w')
+f.write(t1[0] + i0 + t1[1])
+f.close()
+
+#read ongoing and write ongoing.html
+f = open('ongoing_projects', mode = 'r')
+i0 = f.read()
+f.close()
+f = open('ongoing_projects.html', mode = 'w')
 f.write(t1[0] + i0 + t1[1])
 f.close()
